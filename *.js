62866c4cdcd09ea23b6feb7f069fb5d68a9b1513
Ambikesh-shukla <gamerbowofficial@gmail.com>
2026-04-28 20:59:27 +0530

    Update *.js

--- *.js
+++ *.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import('./discord-bot/index.js').catch((error) => {
+import('./discord-bot/*.js').catch((error) => {
   console.error('[launcher] Failed to start discord-bot/*.js');
   console.error(error);
   process.exit(1);
